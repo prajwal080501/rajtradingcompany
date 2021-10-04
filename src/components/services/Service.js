43773f0d7ae605a074605170e582138d770329bf
prajwal080501 <praju.ladkat@gmail.com>
2021-10-04 12:17:47 +0530

    service contact section modified

--- src/components/services/Service.js
+++ src/components/services/Service.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { CardGroup } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import "./Service.css"
 
 const Service = () => {
   return (
     <div>
-      <h1 className="title">Services</h1>
+      <h1 className="uk-title">Services</h1>
       <CardGroup>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src="https://source.unsplash.com/1600x900/?nature,water" />
           <Card.Body>
             <Card.Title>Resiential Painting</Card.Title>
             <Card.Text>
@@ -17,12 +18,10 @@ const Service = () => {
               longer.
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
+          
         </Card>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src="https://source.unsplash.com/1600x900/?nature,water" />
           <Card.Body>
             <Card.Title>Industrial Painting</Card.Title>
             <Card.Text>
@@ -30,12 +29,10 @@ const Service = () => {
               additional content.{" "}
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
+          
         </Card>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src="https://source.unsplash.com/1600x900/?nature,water" />
           <Card.Body>
             <Card.Title>POP & Scaffolding</Card.Title>
             <Card.Text>
@@ -44,9 +41,7 @@ const Service = () => {
               than the first to show that equal height action.
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
+         
         </Card>
       </CardGroup>
     </div>
