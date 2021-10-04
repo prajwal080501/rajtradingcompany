39f8f7e1ff0f63e246e785b331f23c7c87847185
prajwal080501 <praju.ladkat@gmail.com>
2021-10-04 10:46:29 +0530

    Added contact card in contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import "./Contact.css";
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className="uk-container">
+            <div>
+                <h3 className="title">Raj Trading Company</h3>
+                <h className="subtitle">Mail</h>
+                <p>praju.ladkat@gmail.com</p>
+                <h className="subtitle">Phone</h>
+                <p>9834477319</p>
+            </div>
         </div>
     )
 }
